91cebdb14448e9c52f6821973d3bce3dab2a50bb
Martin Jablečník <martin.jablecnik@email.cz>
2026-05-22 12:34:22 +0200

    Fix network request headers

--- src/utils/network.ts
+++ src/utils/network.ts
@@ -18,6 +18,17 @@ export const resolveRedirectUrl = async (
     const { cookies, ...axiosOptions } = options || {};
 
     const headers: Record<string, string> = {
+      accept:
+        'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+      'accept-encoding': 'gzip, deflate, br',
+      'accept-language': 'en-US,en;q=0.6',
+      'cache-control': 'max-age=0',
+      'sec-fetch-dest': 'document',
+      'sec-fetch-mode': 'navigate',
+      'sec-fetch-site': 'none',
+      'sec-fetch-user': '?1',
+      'sec-gpc': '1',
+      'upgrade-insecure-requests': '1',
       'user-agent':
         'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
     };
